c7c75a2d54c5a6e5ceb0477189534eece727bfdc
Maks <hinchak.maksym@chnu.edu.ua>
2026-03-17 15:31:49 +0200

    refactor + method shell

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-
+// Метод бульбашки
 void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
@@ -14,11 +14,46 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+// Метод Шелла
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+
+            arr[j] = temp;
+        }
+    }
+}
+
 int main() {
     int arr[] = { 5, 2, 9, 1, 3 };
     int n = 5;
 
-    bubbleSort(arr, n); 
+    int choice;
+
+    cout << "Оберіть метод сортування:\n";
+    cout << "1 - Бульбашка\n";
+    cout << "2 - Шелла\n";
+    cout << "Ваш вибір: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        bubbleSort(arr, n);
+        cout << "\nМетод бульбашки\n";
+    }
+    else if (choice == 2) {
+        shellSort(arr, n);
+        cout << "\nМетод Шелла\n";
+    }
+    else {
+        cout << "Неправильний вибір!\n";
+        return 1;
+    }
 
     cout << "Відсортований масив: ";
     for (int i = 0; i < n; i++) {
@@ -27,6 +62,3 @@ int main() {
 
     return 0;
 }
-
-
-

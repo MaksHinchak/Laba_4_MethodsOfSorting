771598447bdb772a866d8d312720a0e192202aad
Максим Фочук <fochuk.maksym@chnu.edu.ua>
2026-03-17 15:37:26 +0200

    metod xoara (2metod)

--- main.cpp
+++ main.cpp
@@ -30,6 +30,37 @@ void shellSort(int arr[], int n) {
     }
 }
 
+// Метод Хоара (розбиття)
+int partition(int arr[], int left, int right) {
+    int pivot = arr[(left + right) / 2];
+    int i = left;
+    int j = right;
+
+    while (i <= j) {
+        while (arr[i] < pivot) i++;
+        while (arr[j] > pivot) j--;
+
+        if (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+// Швидке сортування (Хоара)
+void quickSort(int arr[], int left, int right) {
+    if (left >= right) return;
+
+    int index = partition(arr, left, right);
+
+    quickSort(arr, left, index - 1);
+    quickSort(arr, index, right);
+}
+
 int main() {
     int arr[] = { 5, 2, 9, 1, 3 };
     int n = 5;
@@ -39,6 +70,7 @@ int main() {
     cout << "Оберіть метод сортування:\n";
     cout << "1 - Бульбашка\n";
     cout << "2 - Шелла\n";
+    cout << "3 - Хоара\n";
     cout << "Ваш вибір: ";
     cin >> choice;
 
@@ -49,6 +81,10 @@ int main() {
     else if (choice == 2) {
         shellSort(arr, n);
         cout << "\nМетод Шелла\n";
+    }
+    else if (choice == 3) {
+        quickSort(arr, 0, n - 1);
+        cout << "\nМетод Хоара\n";
     }
     else {
         cout << "Неправильний вибір!\n";
